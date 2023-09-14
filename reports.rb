530a40b62c4e1b4139c141694664d12e06fd343a
Rick Gladwin <rick@iddesign.ca>
2023-09-14 12:24:51 -0400

    add reports and tokens modules (incomplete/tentative)

---
+++ reports.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Handles generation and output of reports
+module Reports
+  def generate_user_token_update_report
+
+  end
+end
---
+++ tokens.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Manages token balances and updates
+module Tokens
+
+end
