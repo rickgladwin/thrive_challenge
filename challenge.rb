ee96e63e3d90ab1a0ded57d125bcaf4eb43ffd67
Rick Gladwin <rick@iddesign.ca>
2023-09-15 11:37:12 -0400

    code cleanup

--- challenge.rb
+++ challenge.rb
@@ -7,22 +7,16 @@ require_relative 'email'
 require_relative 'reports'
 
 # Procedure
-# create company objects
 all_companies = Company.all
 puts("all_companies: #{all_companies}")
 
 # create batch report
 token_update_report_batch = Reports::BatchCompanyUsersTokenUpdate.new
 
-# for each company
 all_companies.each do |company|
-  #   create company report element
   company_report = Reports::CompanyUsersTokenUpdate.new(company)
-  #   get company active users
   company_active_users = company.active_users
-  #   for each active user
   company_active_users.each do |user|
-    #     update user token balance
     previous_token_balance = user.tokens
     new_token_balance      = previous_token_balance + company.top_up
     #     optionally email user
@@ -32,7 +26,6 @@ all_companies.each do |company|
       Email.send(email_content)
       user_emailed = true
     end
-    #     update company report element
     company_report.add_user(user: user, user_emailed: user_emailed, old_token_balance: previous_token_balance, new_token_balance: new_token_balance)
   end
 
@@ -43,12 +36,6 @@ end
 puts("batch_report: #{token_update_report_batch.company_updates}")
 
 # present batch report (as output.txt)
-
-# if __FILE__ == $0
-#   puts('--  Company Users Token Updater   --')
-#   puts('   (press any key to begin batch)')
-#   gets
-#
-#   puts('done.')
-#   puts('Output file is available at: output/output.txt')
-# end
+# TODO: generate text from batch report
+# TODO: create report output file
+# TODO: add error handling
