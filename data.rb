64c122fbc91569f62787fcb88de76ce7bbc1650b
Rick Gladwin <rick@iddesign.ca>
2023-09-15 14:03:19 -0400

    delete unused Data and Tokens modules – their responsibilities are handled elsewhere

--- data.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Manages persistent data and provides a facade for data functions
-module Data
-
-end
--- tokens.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Manages token balances and updates
-module Tokens
-
-end
